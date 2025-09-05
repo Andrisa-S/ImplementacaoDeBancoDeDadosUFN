eac9fed5994290880143f07a5bd3da3ba6ec8b88
Andrisa dos Santos Silva <148649363+Andrisa-S@users.noreply.github.com>
2025-09-05 10:40:27 -0300

    Update EMPRESA.sql

--- EmpresaIBD/EMPRESA.sql
+++ EmpresaIBD/EMPRESA.sql
@@ -1,4 +1,4 @@
---CriaÁ„o da tabela FUNCIONARIO
+--Cria√ß√£o da tabela FUNCIONARIO
 CREATE TABLE FUNCIONARIO (
 	Pnome VARCHAR(15) NOT NULL,
     Minicial CHAR,
@@ -26,13 +26,13 @@ CREATE TABLE DEPARTAMENTO (
     FOREIGN KEY (Cpf_gerente) REFERENCES FUNCIONARIO(CPF)
 );
 
---Adiconando restiÁ„o referencial em FUNCIONARO de DEPARTAMENTO
---Criando uma ALTERA«√O de Tabela
+--Adiconando resti√ß√£o referencial em FUNCIONARO de DEPARTAMENTO
+--Criando uma ALTERA√á√ÉO de Tabela
 ALTER TABLE FUNCIONARIO
 ADD CONSTRAINT Dnr
 FOREIGN KEY (Dnr) REFERENCES DEPARTAMENTO (Dnumero);
 
---CriaÁ„o da tabela de LOCALIZACAO_DEP
+--Cria√ß√£o da tabela de LOCALIZACAO_DEP
 CREATE TABLE LOCALIZACAO_DEP (
 	Dnumero INT NOT NULL,
 	Dlocal VARCHAR (15) NOT NULL,
@@ -51,7 +51,7 @@ CREATE TABLE PROJETO(
     FOREIGN KEY (Dnum) REFERENCES DEPARTAMENTO (Dnumero)
 );
 
---CriaÁ„o da tabela TRABALHA_EM
+--Cria√ß√£o da tabela TRABALHA_EM
 CREATE TABLE TRABALHA_EM(
 	Fcpf CHAR(11) NOT NULL,
     Pnr INT NOT NULL,
@@ -61,7 +61,7 @@ CREATE TABLE TRABALHA_EM(
     FOREIGN KEY (Pnr) REFERENCES PROJETO(Projnumero)
 );
 
---CriaÁ„o da tabela DEPENDENTE sem seleÁ„o do esquema
+--Cria√ß√£o da tabela DEPENDENTE sem sele√ß√£o do esquema
 CREATE TABLE DEPENDENTE(
 	Fcpf CHAR(11) NOT NULL,
     Nome_dependente VARCHAR(15) NOT NULL,
@@ -74,26 +74,26 @@ CREATE TABLE DEPENDENTE(
 
 --Inserindo valores no Departamento
 INSERT INTO DEPARTAMENTO (Dnome, Dnumero) VALUES('Pesquisa', 5);
-INSERT INTO DEPARTAMENTO (Dnome, Dnumero) VALUES('AdministraÁ„o', 4);
+INSERT INTO DEPARTAMENTO (Dnome, Dnumero) VALUES('Administra√ß√£o', 4);
 INSERT INTO DEPARTAMENTO (Dnome, Dnumero) VALUES('Matriz', 1);
 SELECT * FROM DEPARTAMENTO;
 
 --Inserindo funcionarios com cargo de gerencia DATE AAAA-MM-DD
-INSERT INTO FUNCIONARIO VALUES ( 'Jorge', 'E', 'Brito', '88866555576', '1937-11-10', 'Rua do Horto, 35, S„o Paulo, SP', 'M', 55000, NULL , 1 );
-INSERT INTO FUNCIONARIO VALUES ( 'Jennifer', 'S', 'Souza', '98765432168', '1941-06-20', 'Av Arthur de Lima, 54, Santo AndrÈ, SP', 'F', 43000, '88866555576' , 4 );
-INSERT INTO FUNCIONARIO VALUES ( 'Fernando', 'T', 'Wong', '33344555587', '1955-12-08', 'Rua da Lapa, 34, S„o Paulo, SP', 'M', 40000, '88866555576' , 5 );
-INSERT INTO FUNCIONARIO VALUES ( 'Jo„o', 'B', 'Silva', '12345678966', '1965-01-09', 'Rua das Flores, 751, S„o Paulo, SP', 'M', 30000, '33344555587' , 5 );
+INSERT INTO FUNCIONARIO VALUES ( 'Jorge', 'E', 'Brito', '88866555576', '1937-11-10', 'Rua do Horto, 35, S√£o Paulo, SP', 'M', 55000, NULL , 1 );
+INSERT INTO FUNCIONARIO VALUES ( 'Jennifer', 'S', 'Souza', '98765432168', '1941-06-20', 'Av Arthur de Lima, 54, Santo Andr√©, SP', 'F', 43000, '88866555576' , 4 );
+INSERT INTO FUNCIONARIO VALUES ( 'Fernando', 'T', 'Wong', '33344555587', '1955-12-08', 'Rua da Lapa, 34, S√£o Paulo, SP', 'M', 40000, '88866555576' , 5 );
+INSERT INTO FUNCIONARIO VALUES ( 'Jo√£o', 'B', 'Silva', '12345678966', '1965-01-09', 'Rua das Flores, 751, S√£o Paulo, SP', 'M', 30000, '33344555587' , 5 );
 INSERT INTO FUNCIONARIO VALUES ( 'Alice', 'J', 'Zelaya', '99988777767', '1968-01-19', 'Rua Souza Lima, 35, Curitiba, PR', 'F', 25000, '98765432168' , 4 );
-INSERT INTO FUNCIONARIO VALUES ( 'Ronaldo', 'K', 'Lima', '66688444476', '1962-09-15', 'Rua RebouÁas, 65, Piracicaba, SP', 'M', 38000, '33344555587' , 5 );
-INSERT INTO FUNCIONARIO VALUES ( 'Joice', 'A', 'Leite', '45345345376', '1972-07-31', 'Av. Lucas Obes, 74, S„o Paulo, SP', 'F', 25000, '33344555587' , 5 );
-INSERT INTO FUNCIONARIO VALUES ( 'AndrÈ', 'E', 'Brito', '98798798733', '1969-03-29', 'Rua Timbira, 35, S„o Paulo, SP', 'M', 25000, '98765432168' , 4 );
+INSERT INTO FUNCIONARIO VALUES ( 'Ronaldo', 'K', 'Lima', '66688444476', '1962-09-15', 'Rua Rebou√ßas, 65, Piracicaba, SP', 'M', 38000, '33344555587' , 5 );
+INSERT INTO FUNCIONARIO VALUES ( 'Joice', 'A', 'Leite', '45345345376', '1972-07-31', 'Av. Lucas Obes, 74, S√£o Paulo, SP', 'F', 25000, '33344555587' , 5 );
+INSERT INTO FUNCIONARIO VALUES ( 'Andr√©', 'E', 'Brito', '98798798733', '1969-03-29', 'Rua Timbira, 35, S√£o Paulo, SP', 'M', 25000, '98765432168' , 4 );
 
---Corrigindo erro de inserÁ„o
+--Corrigindo erro de inser√ß√£o
 UPDATE FUNCIONARIO
 SET Endereco = 'Rua Reboucas, 65, Piracicaba, SP'
 WHERE Cpf = '66688444476';
 
---Recupera todas as informaÁıes de funcion·rios
+--Recupera todas as informa√ß√µes de funcion√°rios
 SELECT * FROM FUNCIONARIO;
 
 --Finzalindo o preenchimento da tabela DEPTARTAMENTO
@@ -106,26 +106,26 @@ WHERE Dnumero = 4;
 UPDATE DEPARTAMENTO
 SET Cpf_gerente = '88866555576', Data_inicio_gerente = '1981-06-19'
 WHERE Dnumero = 1;
---Recuperar todas as informaÁıes de departamento
+--Recuperar todas as informa√ß√µes de departamento
 SELECT * FROM DEPARTAMENTO;
 
 --Prrencher a tabela LOCALIZACAO_DEP
-INSERT INTO LOCALIZACAO_DEP VALUES (1, 'S„o Paulo');
-INSERT INTO LOCALIZACAO_DEP VALUES (4, 'Mau·');
-INSERT INTO LOCALIZACAO_DEP VALUES (5, 'Santo AndrÈ');
+INSERT INTO LOCALIZACAO_DEP VALUES (1, 'S√£o Paulo');
+INSERT INTO LOCALIZACAO_DEP VALUES (4, 'Mau√°');
+INSERT INTO LOCALIZACAO_DEP VALUES (5, 'Santo Andr√©');
 INSERT INTO LOCALIZACAO_DEP VALUES (5, 'Itu');
-INSERT INTO LOCALIZACAO_DEP VALUES (5, 'S„o Paulo');
---Recuperando informaÁıes de LOCALIZACAO_DEP
+INSERT INTO LOCALIZACAO_DEP VALUES (5, 'S√£o Paulo');
+--Recuperando informa√ß√µes de LOCALIZACAO_DEP
 SELECT * FROM LOCALIZACAO_DEP;
 
 --Preenchendo a table PROJETO
-INSERT INTO PROJETO VALUES ('ProdutoX', 1, 'Santo AndrÈ', 5);
+INSERT INTO PROJETO VALUES ('ProdutoX', 1, 'Santo Andr√©', 5);
 INSERT INTO PROJETO VALUES ('ProdutoY', 2, 'Itu', 5);
-INSERT INTO PROJETO VALUES ('ProdutoZ', 3, 'S„o Paulo', 5);
-INSERT INTO PROJETO VALUES ('InformatizaÁ„o', 10, 'Mau·', 4);
-INSERT INTO PROJETO VALUES ('ReorganizaÁ„o', 20, 'S„o Paulo', 1);
-INSERT INTO PROJETO VALUES ('NovosbenefÌcios', 30, 'Mau·', 4);
---Recuperando informaÁıes de PROJETO
+INSERT INTO PROJETO VALUES ('ProdutoZ', 3, 'S√£o Paulo', 5);
+INSERT INTO PROJETO VALUES ('Informatiza√ß√£o', 10, 'Mau√°', 4);
+INSERT INTO PROJETO VALUES ('Reorganiza√ß√£o', 20, 'S√£o Paulo', 1);
+INSERT INTO PROJETO VALUES ('Novosbenef√≠cios', 30, 'Mau√°', 4);
+--Recuperando informa√ß√µes de PROJETO
 SELECT * FROM PROJETO;
 
 --Preenchento TRABALHA_EM
@@ -174,4 +174,12 @@ VALUES
 ('Paulo', 'D', 'Silva', '98765432400', '1982-11-05', 'Rua D, 101', 'M', 7000.00, '98765432300', 4, '2024-05-15', 500.00);
 
 
+
 */
+
+SELECT * FROM FUNCIONARIO;
+SELECT * FROM DEPARTAMENTO;
+SELECT * FROM LOCALIZACAO_DEP;
+SELECT * FROM PROJETO;
+SELECT * FROM TRABALHA_EM;
+SELECT * FROM DEPENDENTE;
